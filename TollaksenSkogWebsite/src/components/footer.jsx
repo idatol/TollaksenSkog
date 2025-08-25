f7302244144dc2189ba7fc2081eb54b73b011e8a
Ida Tollaksen <ida.k.tollaksen@gmail.com>
2025-08-25 14:59:02 +0200

    update before cleanup

---
+++ TollaksenSkogWebsite/src/components/footer.jsx
@@ -0,0 +1,10 @@
+import logo from '../assets/logo.png';
+
+export default function Footer() {
+  return (
+    <footer>
+        <span><Link><img src={logo} alt="Logo" /></Link></span>
+        <p>© 2025 Tollaksen Skogsscervice. Alle rettigheter reservert.</p>
+    </footer>
+  );
+}
--- TollaksenSkogWebsite/src/components/forside.jsx
+++ TollaksenSkogWebsite/src/components/forside.jsx
@@ -1,13 +1,14 @@
 import img1 from '../assets/img1.jpg'
+import '../assets/styles/forside.css'
 
 export default function Forside() {
   return (
-    <>
+    <section id='forside'>
       <h1>Tollaksen Skogsservice</h1>
       <figure>
           <img src={img1} alt="Skogsarbeider" />
       </figure>
       <p>Tollaksen skog hjelper deg med alt innenfor skogbruk i Rogaland.</p>
-    </>
+    </section>
   );
 }
