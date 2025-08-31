f660a7fd4a7126486d0bd6fcb677ee629fe08e9d
Ida Tollaksen <ida.k.tollaksen@gmail.com>
2025-09-01 00:41:21 +0200

    minor uppdates

--- TollaksenSkogWebsite/src/components/FormSubmissionSuccess.jsx
+++ TollaksenSkogWebsite/src/components/FormSubmissionSuccess.jsx
@@ -4,19 +4,10 @@ export default function FormSubmissionSuccess() {
   return (
     <section id="contact">
       <h2>Kontakt</h2>
-      <p id='conp'>Ta kontakt på e-post, telefon eller via skjemaet under.</p>
-      
-      <div>
-        <h3>E-post</h3>
-        <a className="contact-link" href="mailto:tollaksenskogservice@gmail.com"> tollaksenskogservice@gmail.com </a>
-      </div>
-
-      <div>
-        <h3>Telefon</h3>
-        <a className="contact-link" href="tel:+4793604721" aria-label="Ring 936 04 721"> 936 04 721 </a>
-      </div>
-
-      <p>Takk for din henvendelse! Jeg svarer deg så snart som mulig.</p>
+      <article id='f-success'>
+        <p>Takk for din henvendelse!</p>
+        <p>Jeg svarer deg så snart som mulig.</p>
+      </article>
     </section>
   )
 }
