6b6a3f5a927ed2871d4d9995ec3d8be309c5fda8
Ida Tollaksen <ida.k.tollaksen@gmail.com>
2025-09-04 16:19:03 +0200

    added more to sanety setup

--- TollaksenSkogWebsite/src/components/Projects.jsx
+++ TollaksenSkogWebsite/src/components/Projects.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+import { client } from "../sanityClient";
+
 export default function Projects() {
+    const [jobs, setJobs] = useState([]);
+
+    useEffect(() => {
+    client.fetch(`*[_type == "job"]{title, date, summary, "imageUrl": image.asset->url}`)
+      .then((data) => setJobs(data))
+      .catch(console.error);
+  }, []);
+
     return(
         <div>
             <h2>Prosjekter:</h2>
+            <ul>
+                {jobs.map((job) => (
+                    <li key={job.title}>
+                        <h3>{job.title}</h3>
+                        <p>{job.summary}</p>
+                        <p>{job.date}</p>
+                        {job.imageUrl && <img src={job.imageUrl} alt={job.title} />}
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
---
+++ TollaksenSkogWebsite/src/sanityClient.js
@@ -0,0 +1,8 @@
+import { createClient } from '@sanity/client';
+
+export const client = createClient({
+  projectId: 'z8upy87y',
+  dataset: 'production',
+  apiVersion: '2025-09-04',
+  useCdn: true
+});
