c598be0a8e2f47e5498c89d559ff7dcc5c46645d
Ida Tollaksen <ida.k.tollaksen@gmail.com>
2025-09-23 13:34:04 +0200

    typo

--- TollaksenSkogWebsite/src/main.jsx
+++ TollaksenSkogWebsite/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './assets/styles/styles.css'
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
